aa0bb9c80d2e33c111bf7bcf5dae9467b0a36639
Dheerajk1994 <dheerajk.har@gmail.com>
2019-02-07 15:27:29 -0800

    good progress on contacts command

--- commands/simple/contacts.js
+++ commands/simple/contacts.js
@@ -34,23 +34,54 @@ class ContactsCommand  extends commando.Command{
                 'delete <name>      //deletes an entry'
                 );
             }
-            if(msgArgs[1] == 'add' && msgArgs.length === 3){//ADDS A ENTRY
+            if(msgArgs[1] == 'add' && msgArgs.length >= 5){//ADDS AN ENTRY
                 let sql;
-                sql = `INSERT INTO contactinfo (nickname) VALUES ('${msgArgs[2]}')`;
+                sql = `INSERT INTO contactinfo (nickname, firstname, lastname, phone, email) VALUES ('${msgArgs[2].toLowerCase()}', '${msgArgs[3]}', '${msgArgs[4]}', '${msgArgs[5]}', '${msgArgs[6]}')`;
                 connection.query(sql, function(err, result){
-                    if(err) console.log(err);
-                    else return message.channel.sendMessage("Successfully added " + msgArgs[2] + " to contact. Use '!contacts edit' to edit fields.");                        
+                    if(err){ console.log(err); message.channel.sendMessage("ERROR: Maybe that name already exists in the db!");}
+                    else { return message.channel.sendMessage("Successfully added " + msgArgs[2].toLowerCase()
+                     + " to contact. Use '!contacts edit' to edit fields."); }                       
                 });
-                return message.channel.sendMessage("Something went wrong!");
+                return;
             }
-            if(msgArgs[1] === 'edit'){//EDITS ENTRY
+            if(msgArgs[1] === 'edit'){//EDITS AN ENTRY
 
             }
-            if(msgArgs[1] === 'get'){//GETS AN ENTRY
-
+            if(msgArgs[1] === 'get' && msgArgs.length === 3){//GETS AN ENTRY
+                let sql = `SELECT * FROM contactinfo WHERE nickname = '${msgArgs[2].toLowerCase}'`;
+                connection.query(sql, function(err, result, fields){
+                    if(err){
+                        onsole.log(err); 
+                        message.channel.sendMessage("Uh oh! Couldnt find that nickname in the db!");
+                    }
+                    else{
+                        var msg = '';
+                        /*msg += "Firstname: " + result.firstname + '\n' +
+                               "Lasttname: " + result.lastname + '\n' + 
+                               "Phone    : " + result.phone + '\n' +
+                               "Email    : " + result.email + '\n';*/
+                        message.channel.sendMessage(result[0].nickname);
+                    }
+                });
+                return;
             }
-            if(msgArgs[1] === 'list'){//LISTS ALL ENTRIES
-
+            if(msgArgs[1] === 'listall'){//LIST ALL ENTRIES
+                message.channel.sendMessage("Entries in contacts list:");
+                let sql = 'SELECT * FROM contactinfo ';
+                connection.query(sql, function(err, rows){
+                    if(err){
+                        console.log(err); 
+                        message.channel.sendMessage("error");
+                    }
+                    else
+                    var i;
+                    var msg = '';
+                    for(i = 0; i < rows.length; i++){
+                        msg += rows[i].nickname + '\n';
+                    }
+                    message.channel.sendMessage(msg);
+                });
+                return;
             }
             if(msgArgs[1] === 'delete'){//DELETES AN ENTRY
 
